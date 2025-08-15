c802cb522cb6e687ab0c8967854aa382adbf8bb6
Tu NatGuzB <natalia.guzman@genstudents.org>
2025-08-15 12:14:29 -0500

    Se actualiza primera seccion

--- script.js
+++ script.js
@@ -17,3 +17,29 @@ function scrollToSection(sectionId) {
             section.scrollIntoView({ behavior: 'smooth' });
     }
     }
+
+  
+function orbitPlanet(planetId, radius, duration) {
+    anime({
+        targets: `#${planetId}`,
+        translateX: {
+          value: function (_, i) {
+            return radius;
+          }
+        },
+        rotate: '1turn',
+        duration: duration,
+        easing: 'linear',
+        loop: true,
+        direction: 'normal',
+        update: function(anim) {
+          let angle = anim.progress / 100 * 2 * Math.PI;
+          let planet = document.getElementById(planetId);
+          planet.style.transform = `translate(${radius * Math.cos(angle)}px, ${radius * Math.sin(angle)}px)`;
+        }
+      });
+}
+
+    orbitPlanet('earth', 100, 8000);
+    orbitPlanet('mars', 150, 12000);
+    orbitPlanet('jupiter', 210, 20000);
